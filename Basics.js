55dd4ef498db347d02b5af32c9b4870dcd6e549e
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-06-03 15:06:29 +0530

    minor changes

---
+++ Basics.js
@@ -0,0 +1,11 @@
+let accountId =23;
+var accountEmail ="bhurewallabhesh05@gmail.com"
+
+
+/*
+  Prefer let instead of var 
+  because it reduced block scope and functional scope 
+
+
+*/
+console.table([accountEmail,accountId])
