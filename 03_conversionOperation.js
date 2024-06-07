e63c6c7b519fb8215f5b51e25e4e4f4de7584086
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-06-07 15:19:23 +0530

    conversionoperation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,16 @@
+// let score = "hitesh" ;
+
+// console.log(typeof score);
+// console.log(score);
+
+
+let IsLogged="labhesh";
+
+let booleanIsLogged=Boolean(IsLogged);
+console.log(booleanIsLogged);
+
+let someNumber=33;
+
+let stringNumber =String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
