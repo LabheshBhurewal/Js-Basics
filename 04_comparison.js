4e4d0b279b340b37c951379b4e9138e3ea728e2e
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-06-17 15:50:15 +0530

    strings

--- 04_comparison.js
+++ 04_comparison.js
@@ -1,7 +1,12 @@
-console.log(null <= 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-console.log(undefined >=0);
-console.log(undefined ==0);
-console.log(undefined <=0);
+// console.log(undefined >=0);
+// console.log(undefined ==0);
+// console.log(undefined <=0);
+
+
+// const isLogged=true;
+
+console.log(isLogged);
---
+++ 06_strings.js
@@ -0,0 +1,29 @@
+const name = "labhesh"
+const repoCount =30;
+
+console.log(name + repoCount);
+
+console.log(`my name is labhesh ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('hiteshhc')
+
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('t'));
+
+
+const newString =gameName.substring(0,4);
+console.log(newString);
+
+const anotherString =gameName.slice(-7,3);
+console.log(anotherString);
+
+const newStringOne = "  Labhesh  "
+console.log(newStringOne.trim);
+console.log(newStringOne.trim());
+
+
+const url = "https://roadmap.sh/frontend?r=frontend-beginner";
+
+
+console.log(url.includes('sundar'));
