89e03a0584676089120319813f8ae84c16abb44d
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-06-17 16:23:25 +0530

    stack and heap

---
+++ 05_stackandHeap.js
@@ -0,0 +1,19 @@
+// stack (Primitive) Heap (Non-Primitive)
+
+let myYoutubeName = "labheshBhurewaldotcom";
+
+let anotherName = myYoutubeName;
+anotherName="pareshdeshpandedotcom"
+// console.log(anotherName);
+
+let userOne = {
+    email:"bhurewallabhesh05@gmailcom",
+    id:5
+
+}
+
+let userTwo=userOne
+
+userTwo.email="pareshDeshpandegmail.com";
+console.log(userTwo.email);
+console.log(userOne.email);
