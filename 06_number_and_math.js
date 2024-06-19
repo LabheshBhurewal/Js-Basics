98f97313a9b3a71290464187afffce692f5ea725
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-06-19 12:59:58 +0530

    number and math

---
+++ 06_number_and_math.js
@@ -0,0 +1,22 @@
+const balance = 123;
+
+const score =new Number(345.67);
+
+console.log(score);
+
+console.log(score.toFixed());
+
+console.log(Math.floor(4.8));
+console.log(Math.ceil(4.8));
+console.log(Math.abs(-4.8));
+console.log(Math.min(-4.8,8,4,9));
+console.log(Math.max(-4.8,8,4,9));
+
+
+console.log(Math.floor(Math.random()));
+console.log(Math.ceil(Math.random()));
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random() * (max -min +1)) + min );
