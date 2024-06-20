c0c220c15d5032ae3e3c0dab0954bde2d770beb2
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-06-20 15:09:46 +0530

    Date and time

---
+++ 07_DateandTime.js
@@ -0,0 +1,16 @@
+let myDate = new Date();
+
+// console.log(myDate.toDateString());
+
+let myCreatedDate = new Date("1-04-2023")
+console.log(myCreatedDate.toLocaleString());
+
+
+let newDate = new Date();
+
+console.log(newDate);
+// console.log(Math.ceil(Date.now()/1000));
+
+console.log(newDate.getTime());
+console.log(newDate.getDate());
+console.log(newDate.getDay());
