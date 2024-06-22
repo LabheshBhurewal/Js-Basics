d68abfe253fd5761eb407838e625c1f3b597b971
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-06-22 15:32:18 +0530

    minor changes

---
+++ 01_JS_Basics/01_Array.js
@@ -0,0 +1,20 @@
+const myarr = new Array(0,1,2,3,45,88.64);
+
+console.log(myarr[2]);
+
+// myarr.push(9);
+// console.log(myarr);
+
+// // myarr.unshift(9);
+// console.log(myarr);
+ 
+// const newarr = myarr.join();
+// console.log(newarr); 
+
+console.log("A",myarr);
+
+const myn = myarr.slice(1,3);
+console.log(myn);
+
+const myn1 =myarr.splice(1,3);
+console.log(myn1);
