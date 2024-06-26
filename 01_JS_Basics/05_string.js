b17adfb44cba2d350e969f93edbf2dbdac388e49
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-06-26 10:46:06 +0530

    object de-structure and Json format

---
+++ 01_JS_Basics/05_string.js
@@ -0,0 +1,23 @@
+const user={
+    name:"labhesh Bhurewal",
+    isLoggedIn:true,
+    email:"bhurewallabhesh05@gmail.com",
+
+}
+
+const {email}=user;
+
+console.log(email);
+
+// JSON API INTRO
+
+[
+    {},
+    {},
+    {}
+]
+
+{
+    [],
+    []
+}
